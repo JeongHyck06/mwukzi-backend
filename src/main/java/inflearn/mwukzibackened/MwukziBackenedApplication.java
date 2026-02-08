75ab0563bcbda47cefc4428eae12800bd1ea218e
Jack <devjack050700@gmail.com>
2026-02-09 01:45:56 +0900

    init: spring 프로젝트 생성

---
+++ src/main/java/inflearn/mwukzibackened/MwukziBackenedApplication.java
@@ -0,0 +1,13 @@
+package inflearn.mwukzibackened;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MwukziBackenedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MwukziBackenedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inflearn/mwukzibackened/MwukziBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.mwukzibackened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MwukziBackenedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
