2d26517a5aa542787c6ce69c9d77d04eb99e18bf
Jack <devjack050700@gmail.com>
2026-02-10 03:15:57 +0900

    feat/#3: 방 기준 삭제/ 방장 조회 메서드 추가

--- src/main/java/jack/mwukzibackened/domain/participant/ParticipantRepository.java
+++ src/main/java/jack/mwukzibackened/domain/participant/ParticipantRepository.java
@@ -4,11 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
     List<Participant> findByRoomId(UUID roomId);
     boolean existsByRoomIdAndDisplayName(UUID roomId, String displayName);
+    Optional<Participant> findByRoomIdAndUserId(UUID roomId, UUID userId);
     void deleteByRoomId(UUID roomId);
 }
